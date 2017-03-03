90fa34c5e34bed5def41caea4340f20e1dd805e1
Ryan Calder <ryan@jgquinn.com>
2017-03-03 15:41:39 -0600

    add bootstrap 3

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -9,6 +9,8 @@ import { SeedAdvancedConfig } from './seed-advanced.config';
 export class ProjectConfig extends SeedAdvancedConfig {
 
   PROJECT_TASKS_DIR = join(process.cwd(), this.TOOLS_DIR, 'tasks', 'project');
+  FONTS_DEST = `${this.APP_DEST}/fonts`;
+  FONTS_SRC : string[] = [];
 
   constructor() {
     super();
@@ -21,6 +23,7 @@ export class ProjectConfig extends SeedAdvancedConfig {
     this.NPM_DEPENDENCIES = [
       ...this.NPM_DEPENDENCIES,
       // {src: 'jquery/dist/jquery.min.js', inject: 'libs'},
+      { src: 'bootstrap/dist/css/bootstrap.min.css', inject: true }
     ];
 
     // Add `local` third-party libraries to be injected/bundled.
@@ -28,6 +31,11 @@ export class ProjectConfig extends SeedAdvancedConfig {
       ...this.APP_ASSETS,
       // {src: `${this.APP_SRC}/your-path-to-lib/libs/jquery-ui.js`, inject: true, vendor: false}
       // {src: `${this.CSS_SRC}/path-to-lib/test-lib.css`, inject: true, vendor: false},
+    ];
+
+    this.FONTS_SRC = [
+      ...this.FONTS_SRC,
+      'node_modules/bootstrap/dist/fonts/**'
     ];
 
     // Add packages (e.g. ng2-translate)
